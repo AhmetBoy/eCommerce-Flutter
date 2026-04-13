b0a062a38ccd04df586083b4b3468c5c8fd9615e
ahmet <ahmetboy@gmail.com>
2026-04-13 22:01:25 +0300

    debug yazısı kaldırıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mini Katalog Uygulaması',
       theme: ThemeData(
         elevatedButtonTheme: ElevatedButtonThemeData(
